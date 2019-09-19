26489f5d3cd3980abf67618bccfdf86ffea12e6e
Martin Sander-Thomsen <cph-ms782@cphbusiness.dk>
2019-09-19 10:45:26 +0200

    update

---
+++ The-facade-and-the-matching-endpoints/src/main/java/dto/PersonsDTO.java
@@ -0,0 +1,33 @@
+package dto;
+
+import entities.Person;
+
+/**
+ *
+ * @author msi
+ */
+public class PersonDTO {
+
+    private long id;
+    private String fName;
+    private String lName;
+    private String phone;
+
+    public PersonDTO(Person p) {
+        this.fName = p.getFirstName();
+        this.lName = p.getLastName();
+        this.phone = p.getPhone();
+        this.id = p.getId();
+    }
+
+    public PersonDTO(String fn, String ln, String phone) {
+        this.fName = fn;
+        this.lName = ln;
+        this.phone = phone;
+    }
+
+    public PersonDTO() {
+    }
+
+    
+}
